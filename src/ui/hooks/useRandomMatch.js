1f58485c4df3252632109f26d70b5b64f9cc8deb
Charlie Hay <robertcharleshay@outlook.com>
2021-02-12 01:19:58 -0500

    random match ui

--- src/ui/hooks/useRandomMatch.js
+++ src/ui/hooks/useRandomMatch.js
@@ -1,16 +1,17 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import Redux from "src/lib/Redux"
+import Storage from "src/lib/Storage"
 
 export default function useRandomMatch() {
+  const [uuid, setUuid] = useState(null)
   const pazaak = useSelector((s) => s.pazaak)
-  const ready = Object.keys(pazaak).length > 0
+  const ready = Object.keys(pazaak).length > 0 && uuid
 
   useEffect(() => {
     const timeout = Math.floor(Math.random() * 5000)
-    setTimeout(() => {
-      Redux.createFakeRoom()
-    }, timeout)
+    setTimeout(() => Redux.createFakeRoom(), 0)
+    Storage.get("uuid").then((uuid) => setUuid(uuid))
   }, [])
 
   const quit = async () => {
@@ -19,5 +20,5 @@ export default function useRandomMatch() {
     return navigate("enter_room_code")
   }
 
-  return [pazaak, ready, quit]
+  return [pazaak, ready, uuid, quit]
 }
