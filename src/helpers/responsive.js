09d24c61a06ba2b8248cb98f3a96b017c640adb3
Charlie Hay <robertcharleshay@outlook.com>
2021-02-22 11:24:48 -0500

    jest testing

--- src/helpers/responsive.js
+++ src/helpers/responsive.js
@@ -1,4 +1,5 @@
 import { responsiveFontSize, responsiveHeight, responsiveWidth } from "react-native-responsive-dimensions"
+
 export const rFont = (size) => responsiveFontSize(size)
 export const rHeight = (height) => responsiveHeight(height)
 export const rWidth = (width) => responsiveWidth(width)
